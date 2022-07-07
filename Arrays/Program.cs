cb8982af178d2c058ab355727d0b88f603145476
Troy <wilson.troy.anthony@gmail.com>
2022-07-07 19:33:02 -0400

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] titles; //Declare array
+            titles = new string[3]; //instantianting an empty arra with 3 elements
+
+            titles[0] = "What about Bob";
+            titles[1] = "Groundhog Day";
+            //titles[2] = "";
+
+            foreach(string item in titles)
+            {
+                Console.WriteLine(item);
+            }
+
+            int[] intArray = {1, 2, 3, 4, 5, 6, 15, 43, 10};
+
+            foreach(int item in intArray)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
